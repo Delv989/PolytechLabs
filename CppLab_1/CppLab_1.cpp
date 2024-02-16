0f021f4f010484c298eeb6f4239eb19a650e356a
Delv989 <153451933+Delv989@users.noreply.github.com>
2024-02-16 15:54:33 +0300

    second version

--- CppLab_1/CppLab_1.cpp
+++ CppLab_1/CppLab_1.cpp
@@ -3,35 +3,28 @@
 bool get_coef(double* a, double* b, double* c) {
 
     try {
-        std::cin >> *a >> *b >> *c;
+        std::cin >> *a;
+        if (*a == 0) {
+            std::cout << "Incorrect coef a";
+            return false;
+        }
+        std::cin >> *b;
+        std::cin >> *c;
     }
     catch (const std::istream::failure& ex) {
         std::cerr << "Failed to read coef: " << ex.what() << "\n";
     }
-    if (*a == 0)
-        return false;
+
     return true;
 
 }
 
-bool solve_problem(const double a, const double b, const double c, std::pair<double, double>** roots) {
-    double discriminant = b * b - 4 * a * c;
-
-    if (discriminant >= 0) {
-        std::pair<double, double> new_roots(-b + sqrt(discriminant) / (2 * a), -b - sqrt(discriminant) / (2 * a));
-
-        *roots = &new_roots;
-        return true;
-    }
-    return false;
-}
-
-void print_roots(const std::pair<double, double>* roots, const bool has_root) {
-    if (has_root && roots->first == roots->second) {
-        std::cout << roots->first << "\n";
+void print_roots(const double root1, const double root2, const bool has_root) {
+    if (has_root && root1 == root2) {
+        std::cout << root2 << "\n";
     }
     else if (has_root) {
-        std::cout << roots->first << " " << roots->second << "\n";
+        std::cout << root1 << " " << root2 << "\n";
     }
     else {
         std::cout << "No real roots\n";
@@ -39,15 +32,27 @@ void print_roots(const std::pair<double, double>* roots, const bool has_root) {
 
 }
 
+bool solve_problem(const double a, const double b, const double c, double* root1, double* root2) {
+    double discriminant = b * b - 4 * a * c;
+
+    if (discriminant >= 0) {
+        *root1 = (-b + sqrt(discriminant)) / (2 * a);
+        *root2 = (-b - sqrt(discriminant)) / (2 * a);
+        return true;
+    }
+    return false;
+}
+
+
+
 
 
 int main()
 {
     double a, b, c;
     if (get_coef(&a, &b, &c)) {
-        std::pair<double, double>* roots;
-        bool has_root = solve_problem(a, b, c, &roots);
-        print_roots(roots, has_root);
+        double root1, root2;
+        bool has_root = solve_problem(a, b, c, &root1, &root2);
+        print_roots(root1, root2, has_root);
     }
 }
-
