4eedb9b54703a5e20b76b1a0a0270849b536f2a4
Delv989 <153451933+Delv989@users.noreply.github.com>
2024-03-01 17:24:26 +0300

    fixed

--- CppLab_1/CppLab_1.cpp
+++ CppLab_1/CppLab_1.cpp
@@ -2,18 +2,20 @@
 #include <vector>
 #include <limits>
 
+bool is_equal_zero(double a) {
+    return abs(a) < std::numeric_limits<double>::epsilon();
+}
+
 bool get_coef(double* a, double* b, double* c) {
-    std::cin >> *a;
-    std::cin >> *b;
-    std::cin >> *c;
-    if (a == NULL || b == NULL || c == NULL)
+    if (!(std::cin >> *a >> *b >> *c)) {
+        std::cout << "Wrong input";
         return false;
-
-    if (*a < std::numeric_limits<double>::epsilon() && *b < std::numeric_limits<double>::epsilon()) 
+    }
+    if (is_equal_zero(*a) && is_equal_zero(*b)) {
+        std::cout << "Incorrect coef\n";
         return false;
-    
+    }
     return true;
-
 }
 
 void print_roots(std::vector<double>* const roots) {
@@ -30,18 +32,18 @@ void print_roots(std::vector<double>* const roots) {
 }
 
 void find_roots(const double a, const double b, const double c, std::vector<double>* roots) {
-    double discriminant = b * b - 4 * a * c;
-    if  (a < std::numeric_limits<double>::epsilon()) {
+    double discriminant = b * b - 4.0 * a * c;
+    if  (is_equal_zero(a)) {
         (*roots).push_back(-c / b);
         return;
     }
-    else if (discriminant < std::numeric_limits<double>::epsilon()) {
-        (*roots).push_back(-b / (2 * a));
+    else if (is_equal_zero(discriminant)) {
+        (*roots).push_back(-b / (2.0 * a));
         return;
     }
     else if (discriminant > 0) {
-        (*roots).push_back((-b + sqrt(discriminant)) / (2 * a));
-        (*roots).push_back((-b - sqrt(discriminant)) / (2 * a));
+        (*roots).push_back((-b + sqrt(discriminant)) / (2.0 * a));
+        (*roots).push_back((-b - sqrt(discriminant)) / (2.0 * a));
         return;
     }
 }
@@ -54,8 +56,5 @@ int main()
         std::vector<double> roots;
         find_roots(a, b, c, &roots);
         print_roots(&roots);
-    }
-    else {
-        std::cout << "Incorrect coef\n";
     }
 }
